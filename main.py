f6a35643742ba5f77b00565b46112e32c875b93e
A D <moeknows@gmail.com>
2019-06-26 16:43:45 -0700

    Adding lesson01 homework

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +15,25 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    donors = Donor.select()
+    if request.method == 'GET':
+        return render_template('create.jinja2', donors=donors)
+    donor_name = request.form['donor']
+    donor_amount = int(request.form['amount'])
+    Donation(donor=donor_name, value=donor_amount).save()
+    return redirect(url_for('home'))
+
+@app.route('/donor/', methods=['GET', 'POST'])
+def donor():
+    donors = Donor.select()
+    if request.method == 'GET':
+        return render_template('donor.jinja2')
+    donor_name = request.form['donor']
+    Donor(name=donor_name).save()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
